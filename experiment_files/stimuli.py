6870e6141873511e19983e7e256ae79427b20d06
Zora <znolte@uni-osnabrueck.de>
2021-06-24 15:13:27 +0200

    forgot to commit updated stimuli file

--- experiment_files/stimuli.py
+++ experiment_files/stimuli.py
@@ -4,6 +4,21 @@
 import numpy as np
 from psychopy import visual
 
+# monitor specs global variables
+M_WIDTH = 1920
+M_HEIGHT = 1200
+REFRESH_RATE = 60
+
+# Gabor patch global variables
+CYCLES = 10  # required cycles for the whole patch
+X = 395  # size of texture in pixels, needs to be to the power of 2!
+sf = CYCLES/X
+
+gabortexture = (
+    visual.filters.makeGrating(res=X, cycles=X * sf) *
+    visual.filters.makeMask(matrixSize=X, shape="circle", range=[0, 1])
+)
+
 
 class stimulus:
     def __init__(self, X, window, xoffset, threshold):
