b7c4cb78ccd1cfa9f575282d0b309e9c66b773ca
Natalia Scharfenberg <nscharfenber@uos.de>
2021-07-07 00:49:59 +0200

    Update mask

--- experiment_files/transparency-mask.py
+++ experiment_files/transparency-mask.py
@@ -11,6 +11,4 @@ sigma = 87 # 1.8 degrees of visual angle; the value in pixels is for 75 cm dista
 x = np.zeros((X, X))
 cv2.circle(x, (center,center), radius=radius, color=1, thickness=1)
 x = ndi.filters.gaussian_filter(x, sigma=sigma)
-x = np.interp(x, (x.min(), x.max()), (-1, 1))
-x[np.where(x == 1)] = -1
 np.save('gaussian_ann.npy', x)
