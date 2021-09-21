95c5f55a89b563957131edfeb407916f7243e13f
Natalia Scharfenberg <nscharfenber@uos.de>
2021-09-21 23:03:18 +0200

    Adjust button mappings for 2IFC

--- experiment_files/titration-2ifc.py
+++ experiment_files/titration-2ifc.py
@@ -4,12 +4,22 @@ import json
 import time
 import numpy as np
 import psychopy
-from psychopy import visual, event, core, monitors, data
+from psychopy import visual, event, core, monitors, data, prefs
 from psychopy.data import QuestHandler
 import stimuli
 
+
+# set up sound for beeps
+prefs.hardware['audioLib'] = ['PTB']
+
+from psychopy import sound
+sound.setDevice('USB Audio Device: - (hw:3,0)')
+
+from psychopy.sound import Sound
+beep = Sound('A', secs=0.5, volume=0.1)
+
 # set the number of trials
-numberOfTrials = 75 # should be 100
+numberOfTrials = 75
 
 # Directory Specs
 HOME = os.getcwd()
@@ -52,52 +62,57 @@ subjectData['pair_id'] = pair_id
 def secondstoframes (seconds):
     return range( int( np.rint(seconds * REFRESH_RATE) ) )
 
+# draws a composite fixation point
+def draw_fixation(fixation):
+
+    for grating in fixation:
+        grating.draw()
+
 # stimulus draw functions for signal and noise
 def draw_signal(noise, signal, fixation):
     noise = stimulus.updateNoise()
     noise.draw()
     signal.draw()
     # create composite fixation target
-    for grating in fixation:
-        grating.draw()
+    draw_fixation(fixation)
 
 def draw_noise(noise, fixation):
     noise = stimulus.updateNoise()
     noise.draw()
     # create composite fixation target
-    for grating in fixation:
-        grating.draw()
+    draw_fixation(fixation)
 
+
+
+def geninstrfamiliarization():
+    instructions = "Please read the instructions carefully.\n\
+    1. Place your index finger on the left button and your middle finger on the right button.\n\
+    2. Fixate on the red dot in the center of the screen. \n\
+    3. You will see two intervals of 750ms each. Both of them will show noise and in one there will be stripes on top of the noise.\n\
+    4. Please indicate which interval had the stripes:\n\
+    When you hear the beep, press the red button for the first interval and the green button for the second interval.\n\n\
+    Press the green button to continue"
+
+    visual.TextStim(window,
+                    text=instructions, pos=(0, 0),
+                    color='black', height=20).draw()
+
+
+def geninstrtitration():
+    instructions = "Please read the instructions carefully.\n\n\
+    In this part of the experiment you still have to decide which interval had the stripes. \n\
+    Press the red button if the stripes were in the first interval and the green button if there were stripes in the second interval. \n\
+    Try to be as accurate as possible. \n\
+    Sometimes the stripes will be very pale - please still try to make a decision. \n\n\
+    Press the green button to start the experiment."
+
+    visual.TextStim(window,
+                    text=instructions, pos=(0, 0),
+                    color='black', height=20).draw()
 
 def genendscreen():
     instructions = "You have finished the titration.\n\n\
     Please wait"
-
-    visual.TextStim(window,
-                    text=instructions, pos=(0, 0),
-                    color='black', height=20).draw()
-
-def geninstrtitration():
-    instructions = "Please read the instructions carefully.\n\
-    1. Now we will determine your individual threshold for recognizing the vertical grating.\n\
-    2. The procedure is the same as before: Press the green key if you saw a grating, and the red key if you didn’t.\n\
-    3. Fixate on the red dot in the center of the circle.\n\
-    4. The visibility of the grating will be adjusted throughout the trials.\n\n\
-    Press yes to continue"
-
-    visual.TextStim(window,
-                    text=instructions, pos=(0, 0),
-                    color='black', height=20).draw()
-
-def geninstrfamiliarization():
-    instructions = "Please read the instructions carefully.\n\
-    1. Place your index finger on the left key and your middle finger on the right key.\n\
-    2. First, you will become familiar with the stimulus and the handling of the button box.\n\
-    3. For the stimulus, a red dot is shown in the middle of the screen, surrounded by a circular pattern that looks similar to white noise.\n\
-    4. You need to indicate whether you saw a vertical grating on top of the noise.\n\
-    5. Fixate on the dot in the center of the circular pattern.\n\
-    6. Press the green key for 'yes' and the red key for 'no'.\n\n\
-    Press yes to continue"
 
     visual.TextStim(window,
                     text=instructions, pos=(0, 0),
@@ -119,10 +134,10 @@ while titration_over == False:
     subjectData['chamber'] = chamber
 
     # variables for button box input
-    keys = ["1", "2"] if chamber == "1" else ["7", "8"] # first one is first interval
+    keys = ["1", "2"] if chamber == "1" else ["8", "7"] # first one is first interval
 
     # the screen
-    window = psychopy.visual.Window(size=(M_WIDTH, M_HEIGHT), units='pix', screen=int(chamber), fullscr=False, pos=None, blendMode='add', useFBO=True, monitor=myMon)
+    window = psychopy.visual.Window(size=(M_WIDTH, M_HEIGHT), units='pix', screen=int(chamber), fullscr=True, pos=None, blendMode='add', useFBO=True, monitor=myMon)
     window.mouseVisible = False
 
     # the stimulus
@@ -131,113 +146,35 @@ while titration_over == False:
     noise = stimulus.noise
     fixation = stimulus.fixation_red
 
-    decision_interval = "Press the green button if you saw stripes in the first interval, or the red button if you saw stripes in the second interval."
+    #make black fixation
+    fixation_black = stimulus.fixation_black
+
+    decision_interval = "Press the red button if you saw stripes in the first interval, or the green button if you saw stripes in the second interval."
 
     decision = visual.TextStim(window,
                     text=decision_interval, pos=(0,0),
                     color='black', height=20)
 
-
     '''
     1. Familiarization
     '''
-    key = []
-    signal.opacity = initial_threshold # set contrast to
-
-    # randomly pick which interval has signal
-    signal_int = np.random.randint(2)
-
-    if not signal_int: # signal in first interval
-        # show interval 1 for 750 ms
-        for frame in secondstoframes(0.75):
-            draw_signal(noise, signal, fixation) # draw signal
-            window.flip()
-
-        # cool down of 500 ms
-        for frame in secondstoframes(0.5):
-            window.flip()
-
-        # show interval 2 for 750 ms
-        for frame in secondstoframes(0.75):
-            draw_noise(noise, fixation) # draw noise
-            window.flip()
-    else: # signal in second interval
-        # show interval 1 for 750 ms
-        for frame in secondstoframes(0.75):
-            draw_noise(noise, fixation) # draw noise
-            window.flip()
-
-        # cool down of 500 ms
-        for frame in secondstoframes(0.5):
-            window.flip()
-
-        # show interval 1 for 750 ms
-        for frame in secondstoframes(0.75):
-            draw_signal(noise, signal, fixation) # draw signal
-            window.flip()
-
-    event.clearEvents()
-    # decision interval of 2.5 s
-    for frame in secondstoframes(5):
-        decision.draw()
-        window.flip()
-
-        # get button press
-        if not key:
-            key = event.getKeys(keys)
-        else:
-            break
-
+    geninstrfamiliarization()
     window.flip()
-    core.wait(3) # wait for 3 s before next trial
-
-    '''
-    2. Titration
-    '''
-
-    #the ladder
-
-    if method=="quest":
-        staircase = QuestHandler(
-                                    startVal=initial_threshold,
-                                    startValSd=prior_std,
-                                    pThreshold=0.82, # gives 75 % for 2IFC
-                                    gamma=0.5,
-                                    #stopInterval=0,
-                                    nTrials=numberOfTrials,
-                                    minVal=0.005,
-                                    maxVal=0.1,
-                                    method='quantile'
-                                    )
-    elif method=="staircase":
-        staircase = data.StairHandler(startVal = initial_threshold, nReversals=10,
-                          stepType = 'log', stepSizes=[0.08,0.04,0.04,0.02],
-                          nUp=1, nDown=3,  # will home in on the 80% threshold
-                          nTrials=numberOfTrials, minVal=0, maxVal=1)
-
-    geninstrtitration() # display instructions
-    window.flip()
-    key = event.waitKeys(keyList=keys[:1])
+    key = event.waitKeys(keyList=keys[1])
     window.flip()
 
-    """
-    Main section:
-        1. use the ladder already created to change the contrast of the grating
-        2. Show two intervals to subject
-        3. During the decision interval, collect response
-        4. Pass on response evaluation to ladder (0 if subject responded correctly, 1 if subject did not)
-        5. Do 1 to 4 for ntimes set in ladder constructor
-    """
-    # list that is filled with the staircase values
-    staircase_medians = []
-    staircase_contrasts = []
-
-    for contrast in staircase:
+    for _ in range(10):
         key = []
-        signal.opacity = contrast # set contrast to the next value from the staircase
+        contrast = np.random.normal(loc=initial_threshold,scale=prior_std)
+        signal.opacity = contrast # set contrast to
 
         # randomly pick which interval has signal
         signal_int = np.random.randint(2)
+
+        # Fixation period
+        for frame in secondstoframes(2.5):
+            draw_fixation(fixation_black)
+            window.flip()
 
         if not signal_int: # signal in first interval
             # show interval 1 for 750 ms
@@ -270,6 +207,10 @@ while titration_over == False:
 
         event.clearEvents()
         # decision interval of 2.5 s
+
+        nextflip = window.getFutureFlipTime(clock='ptb')
+        beep.play(when=nextflip)
+
         for frame in secondstoframes(5):
             decision.draw()
             window.flip()
@@ -279,7 +220,107 @@ while titration_over == False:
                 key = event.getKeys(keys)
             else:
                 break
+        beep.stop()
+        window.flip()
+        core.wait(3) # wait for 3 s before next trial
 
+    '''
+    2. Titration
+    '''
+
+    #the ladder
+
+    if method=="quest":
+        staircase = QuestHandler(
+                                    startVal=initial_threshold,
+                                    startValSd=prior_std,
+                                    pThreshold=0.82, # gives 75 % for 2IFC
+                                    gamma=0.5,
+                                    delta=0.05,
+                                    #stopInterval=0,
+                                    nTrials=numberOfTrials,
+                                    minVal=0.005,
+                                    maxVal=0.1,
+                                    method='quantile'
+                                    )
+    elif method=="staircase":
+        staircase = data.StairHandler(startVal = initial_threshold, nReversals=10,
+                          stepType = 'log', stepSizes=[0.08,0.04,0.04,0.02],
+                          nUp=1, nDown=3,  # will home in on the 80% threshold
+                          nTrials=numberOfTrials, minVal=0, maxVal=1)
+
+    geninstrtitration() # display instructions
+    window.flip()
+    key = event.waitKeys(keyList=keys[1])
+    window.flip()
+
+    """
+    Main section:
+        1. use the ladder already created to change the contrast of the grating
+        2. Show two intervals to subject
+        3. During the decision interval, collect response
+        4. Pass on response evaluation to ladder (0 if subject responded correctly, 1 if subject did not)
+        5. Do 1 to 4 for ntimes set in ladder constructor
+    """
+    # list that is filled with the staircase values
+    staircase_medians = []
+    staircase_contrasts = []
+
+    for contrast in staircase:
+        key = []
+        signal.opacity = contrast # set contrast to the next value from the staircase
+
+        # randomly pick which interval has signal
+        signal_int = np.random.randint(2)
+
+        # Fixation period
+        for frame in secondstoframes(2.5):
+            draw_fixation(fixation_black)
+            window.flip()
+
+        if not signal_int: # signal in first interval
+            # show interval 1 for 750 ms
+            for frame in secondstoframes(0.75):
+                draw_signal(noise, signal, fixation) # draw signal
+                window.flip()
+
+            # cool down of 500 ms
+            for frame in secondstoframes(0.5):
+                window.flip()
+
+            # show interval 2 for 750 ms
+            for frame in secondstoframes(0.75):
+                draw_noise(noise, fixation) # draw noise
+                window.flip()
+        else: # signal in second interval
+            # show interval 1 for 750 ms
+            for frame in secondstoframes(0.75):
+                draw_noise(noise, fixation) # draw noise
+                window.flip()
+
+            # cool down of 500 ms
+            for frame in secondstoframes(0.5):
+                window.flip()
+
+            # show interval 1 for 750 ms
+            for frame in secondstoframes(0.75):
+                draw_signal(noise, signal, fixation) # draw signal
+                window.flip()
+
+        event.clearEvents()
+        nextflip = window.getFutureFlipTime(clock='ptb')
+        beep.play(when=nextflip)
+        # decision interval of 2.5 s
+        for frame in secondstoframes(5):
+            decision.draw()
+            window.flip()
+
+            # get button press
+            if not key:
+                key = event.getKeys(keys)
+            else:
+                break
+        beep.stop()
         if keys[signal_int] in key: # check if decision is correct
             response = 1
             print(response)
